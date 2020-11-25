c2b53842f00aee3c20887511d12df5afd6fb35fc
Администратор <@DESKTOP-PHORFLN>
2020-11-25 16:52:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace Practicks_TMP_1
+{
+    class Program
+    {
+        static int MaxInQueue(Queue _queue) // Максимальное число в очереди
+        {
+            Queue tempqueue = new Queue();
+            int temp = 0;
+
+            int max = _queue.Pop();
+            tempqueue.Push(max);
+
+            int size = _queue.QueueSize;
+
+            for (int i = 0; i < size; ++i)
+            {
+                temp = _queue.Pop();
+                tempqueue.Push(temp);
+                if (max < temp)
+                {
+                    max = temp;
+                }
+            }
+
+            size = tempqueue.QueueSize;
+
+            for (int i = 0; i < size; ++i)
+            {
+                _queue.Push(tempqueue.Pop());
+            }
+
+            return max;
+        }
+
+        static int MinInQueue(Queue _queue) // Минимальное число в очереди
+        {
+            Queue tempqueue = new Queue();
+            int temp = 0;
+
+            int min = _queue.Pop();
+            tempqueue.Push(min);
+
+            int size = _queue.QueueSize;
+
+            for (int i = 0; i < size; ++i)
+            {
+                temp = _queue.Pop();
+                tempqueue.Push(temp);
+                if (min > temp)
+                {
+                    min = temp;
+                }
+            }
+
+            size = tempqueue.QueueSize;
+
+            for (int i = 0; i < size; ++i)
+            {
+                _queue.Push(tempqueue.Pop());
+            }
+
+            return min;
+        }
+
+        static void SortQueue(Queue _queue) // Сортировка
+        {
+            int max = MaxInQueue(_queue);
+            int min = MinInQueue(_queue);
+            int[] nums = new int[max + 1];
+
+            // Массив для отрицательных значений
+            int[] nnums = new int[0];
+            if (min < 0)
+            {
+                min = min * -1;
+                nnums = new int[min + 1];
+            }
+
+            for (int i = 0; i < max + 1; ++i)
+            {
+                nums[i] = 0; // Заполняем массив ноликами
+            }
+
+            for (int i = 0; i < min + 1; ++i)
+            {
+                nnums[i] = 0; // Заполняем массив ноликами
+            }
+
+            int size = _queue.QueueSize;
+
+            for (int i = 0; i < size; ++i)
+            {
+                int pop = _queue.Pop();
+                if (pop < 0)
+                {
+                    nnums[pop * -1] += 1;
+                }
+                else
+                {
+                    nums[pop] += 1;
+                }
+            }
+
+            for (int i = min; i >= 0; --i)
+            {
+                for (int j = 0; j < nnums[i]; ++j)
+                {
+                    _queue.Push(i * -1);
+                }
+            }
+
+            for (int i = 0; i < max + 1; ++i)
+            {
+                for (int j = 0; j < nums[i]; ++j)
+                {
+                    _queue.Push(i);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Queue tmp = new Queue();
+            Random randomNumber = new Random();
+
+            Console.WriteLine("Введите количество чисел и интервал генерации (всё через пробел)");
+            string[] input = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < Convert.ToInt32(input[0]); ++i)
+            {
+                tmp.Push(randomNumber.Next(Convert.ToInt32(input[1]), Convert.ToInt32(input[2]))); // Переводим в int интервал 
+            }
+
+            SortQueue(tmp);
+
+            Console.WriteLine("Отсортированная очередь: ");
+            tmp.Print();
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Practicks_TMP_1
+{
+    class Queue
+    {
+        private int[] queue = new int[0];
+
+        public Queue()
+        {
+
+        }
+
+        public int QueueSize //Размер очереди
+        {
+            get
+            {
+                int size = 0;
+
+                foreach (int o in queue)
+                {
+                    ++size;
+                }
+                return size;
+            }
+        }
+
+        public void Push(int _newElement)//Добавляет число _newElement в конец очереди (в начало массива)
+        {
+            int size = QueueSize;
+            int[] temp = new int[size];
+            for (int i = 0; i < size; ++i)
+            {
+                temp[i] = queue[i];
+            }
+
+            queue = new int[size + 1];
+            queue[0] = _newElement;
+
+            for (int i = 1; i < size + 1; ++i)
+            {
+                queue[i] = temp[i - 1];
+            }
+        }
+
+        public int Pop()//Возвращает первый элемент в очереди (последний элемент массива) и при этом удаляет его из этой очереди
+        {
+            int size = QueueSize;
+            int rez = queue[size - 1]; // QueueSize - 1 потому что в массиве элементы считаются с 0
+            int[] temp = new int[size];
+            for (int i = 0; i < size; ++i)
+            {
+                temp[i] = queue[i];
+            }
+            queue = new int[size - 1];
+
+            for (int i = 0; i < size - 1; ++i)
+            {
+                queue[i] = temp[i];
+            }
+
+            return rez;
+        }
+
+        public int Back()//Возвращает первый элемент в очереди (последний в массиве) и НЕ удаляет его из очереди
+        {
+            int rez = queue[QueueSize - 1]; // QueueSize - 1 потому что в массиве элементы считаются с 0
+            return rez;
+        }
+
+        public void Print()
+        {
+            foreach (int item in queue)
+                Console.WriteLine(item);
+            Console.WriteLine("---------");
+        }
+    }
+}
